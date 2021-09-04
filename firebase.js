aa1e06e6ef6bc8869f5bbd24d77230512a52dfaf
Chaitalish02 <chaitalishetty261@gmail.com>
2021-09-04 16:33:03 +0530

    ui 3

--- firebase.js
+++ firebase.js
@@ -14,33 +14,46 @@ const firebaseConfig = {
   let blogs=firebase.database().ref("blogs");
 //   document.getElementById("register").addEventListener("click",registerUser);
   function registerUser(){
-     
+      
       let username=document.getElementById("username").value;
-      let email=document.getElementById("email").value;
       let password=document.getElementById("password").value;
-      firebase.database().ref("users").once("value",function(snapshot){
-            snapshot.forEach(function(childSnapshot){
-                var childKey=childSnapshot.key;
-                var childValue=childSnapshot.val();
-                if (childValue["username"]==username || childValue["email"]==email){
-                   alert("This email is already taken");
-                }
-                else{
-                    saveUserData(username,email,password);
-                }
-            });
-        });
-      function saveUserData(username,email,password){
-          let newUsers=users.push();
-          newUsers.set({
-              username:username,
-              email:email,
-              password:password
-          });
-          alert("Registration successful!!");
-          window.location.href="login.html";
+      let conpass=document.getElementById("conpass").value;
+      function checkPass(){
+        if(username!="" && password!=""){
+            if (password==conpass){
+             firebase.database().ref("users").once("value",function(snapshot){
+                    snapshot.forEach(function(childSnapshot){
+                        var childKey=childSnapshot.key;
+                        var childValue=childSnapshot.val();
+                        if (childValue["username"]==username){
+                        alert("This username is already taken");
+                        }
+                        else{
+                            saveUserData(username,password);
+                        }
+                    });
+                });
+            function saveUserData(username,password){
+                let newUsers=users.push();
+                newUsers.set({
+                    username:username,
+                    password:password
+                });
+                alert("Registration successful!!");
+                document.getElementById("username").value="";
+                window.location.href="login.html";
+            } 
+          }
+          else{
+              document.getElementById("wrongpass").style.display="block";
+          }
+        }
+        else{
+            alert("You need to enter something");
+        }
           
       }
+      checkPass();
       
   }
 
@@ -75,49 +88,5 @@ const firebaseConfig = {
       }
       getUserData();
   }
-  function postBlog(){
-    let heading=document.getElementById("heading").value;
-    let blogtext=document.getElementById("blogtext").value;
-    let checkbox=document.getElementById("checkbox");
-    let status="";
-    if(checkbox.checked==true){
-        status="private"
-    }
-    else{
-        status="public"
-    }
-    if(heading!=" " || blogtext!=" "){
-        blogSubmit();
-    }
-    else{
-        alert("You need to enter something");
-    }
-    function blogSubmit(){
-        let newBlogs=blogs.push();
-        newBlogs.set({
-            heading:heading,
-            date:Date.now(),
-            blogtext:blogtext,
-            status:status,
-            author:sessionStorage.getItem("username")
-        });
-        alert("You blog is saved successfully");
-    }
-
-  }
-  function showPosts(){
-    firebase.database().ref("blogs").once("value",function(snapshot){
-        snapshot.forEach(function(childSnapshot){
-            var childKey=childSnapshot.key;
-            var childValue=childSnapshot.val();
-            if(childValue["status"]=="public"){
-                
-                let displayBlog=childValue["heading"]+childValue["blogtext"]+childValue["date"]
-                document.querySelector(".posts").innerHTML+=displayBlog;
-            }
-            else if(childValue["status"=="private"]){
-                
-            }
-        });
-    });
-}
+  
+ 
